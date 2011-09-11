fa7313a625b9bd6a3e124fbef6bcb55acbfd0dda
Arnaud Berthomier <oz@cyprio.net>
2011-09-12 00:27:49 +0200

    Vibrate when a script is run

--- wiird.py
+++ wiird.py
@@ -13,13 +13,13 @@ from cwiid import Wiimote
 
 class Wiird:
     def __init__(self, mac_address):
-        self.omg_please_stop = False
-        self.wm = cwiid.Wiimote(mac_address)
-        self.wm.rpt_mode = cwiid.RPT_STATUS | cwiid.RPT_BTN
+        self.omg_please_stop  = False
+        self.wm               = cwiid.Wiimote(mac_address)
+        self.wm.rpt_mode      = cwiid.RPT_STATUS | cwiid.RPT_BTN
+        self.wm.mesg_callback = self.mesg_cb
 
         # Light up LEDs 2 and 3 when we're connected
-        self.wm.led = 2 | 4
-        self.wm.mesg_callback = self.mesg_cb
+        self.wm.led           = 2 | 4
 
     # When the wiimote is powered down, this mesg callback is called,
     # and a is [(8, 1)]. I REALLY have no idea why, and not time to dig
@@ -32,12 +32,13 @@ class Wiird:
     def list_scripts(self):
         return [re.sub(r'.*/(.*)\.sh', r'\1', x) for x in glob("./scripts.d/*.sh")]
 
-    # Check self.w.state['buttons'] value agains each scripts.d/ script
-    # defined. The script name is passed as 'handler', it is uppercased
-    # to check:
+    # Check self.wm.state['buttons'] value against each sh script in the
+    # scripts.d/ directory.  The script name is passed as 'handler', it
+    # is uppercased to check:
     #   - wether the constant exists in cwiid
     #   - wether a button event matches
-    # If both are true, the handler script is launched.
+    #
+    # Whenever both are true, the handler script is launched.
     def run_handler(self, handler):
         const_name = handler.upper()
         try:
@@ -61,6 +62,12 @@ class Wiird:
                 break
             sleep(0.1)
 
+    # Vibrate for a while.
+    def vibrate(self, duration):
+        self.wm.rumble = 1
+        sleep(duration)
+        self.wm.rumble = 0
+
     # Run script if it exists in scripts.d/ directory
     def exec_script(self, script):
         filename = "./scripts.d/" + script + ".sh" 
@@ -73,18 +80,21 @@ class Wiird:
         except IOError as e:
             return 0
         subprocess.call(filename)
+        self.vibrate(0.3)
 
-if len(sys.argv) != 2:
-    sys.stderr.write("Usage: wyyrd.py <wiimote mac address>")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: wyyrd.py <wiimote mac address>")
+        exit(1)
 
-wiimote_mac = sys.argv[1] # '00:19:1D:AB:24:7F'
-print 'Put your Wiimote in discoverable mode now (press 1+2)...'
-while True:
-    try:
-        print('Waiting for wiimote ' + wiimote_mac)
-        wm = Wiird(wiimote_mac)
-        print("Ready!")
-        wm.run()
-    except RuntimeError:
-        continue
+    wiimote_mac = sys.argv[1]
+    print('Put your Wiimote in discoverable mode now: press 1+2...')
+    while True:
+        try:
+            print('Waiting for wiimote ' + wiimote_mac)
+            wm = Wiird(wiimote_mac)
+            print("Ready!")
+            wm.run()
+        except RuntimeError:
+            # Never gonna give you up...
+            continue
